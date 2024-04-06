4286e3faa2c5c633af8d137246cfd354c684e1d0
Caio Vitor <caiovitordm@protonmail.com>
2024-04-06 17:34:58 -0300

    fix: inserindo eureka server client no ms-base-code.

---
+++ src/main/java/application/MsBaseCodeApplication.java
@@ -0,0 +1,13 @@
+package Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBaseCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBaseCodeApplication.class, args);
+	}
+
+}
--- src/main/java/application/ServletInitializer.java
+++ src/main/java/application/ServletInitializer.java
@@ -1,13 +1,13 @@
-package application;
+package Application;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MsBaseApplication.class);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MsBaseCodeApplication.class);
+	}
 
 }
